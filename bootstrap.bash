6835f904fca5f3261abe600dcc399cbecee05f49
Anton Eprev <a.eprev@gmail.com>
2014-10-31 10:02:13 +0100

    Added SSH_AUTH_SOCK env

--- bootstrap.bash
+++ bootstrap.bash
@@ -8,6 +8,15 @@ for file in $HOME/.dotfiles/includes/*.bash; do
 done
 unset file
 
+# Set SSH authentication socket location
+SOCK="/tmp/ssh-agent-$USER-screen"
+if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $SOCK ]
+then
+    rm -f /tmp/ssh-agent-$USER-screen
+    ln -sf $SSH_AUTH_SOCK $SOCK
+    export SSH_AUTH_SOCK=$SOCK
+fi
+
 # Setup git completion for its alias
 __git_complete g __git_main
 
